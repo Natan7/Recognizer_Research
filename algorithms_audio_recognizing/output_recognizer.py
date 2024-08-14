dadec1685a79e2dff37f77a918688755e6122f2b
Natan <natan.ribeiro@ccc.ufcg.edu.br>
2024-08-14 14:03:32 -0300

    refatoramento e correções

--- algorithms_audio_recognizing/output_recognizer.py
+++ algorithms_audio_recognizing/output_recognizer.py
@@ -1,7 +1,9 @@
 def output(file_path, file_name, generated_text):
-    with open(file_path + file_name,'w') as f:
-        f.write('{}\n'.format(generated_text))
-
-    print("Transcrição gerada: ")
-    print(file_path + file_name)
-    print()
+    try:
+        with open(file_path + file_name,'w') as f:
+            f.write('{}\n'.format(generated_text))
+        print("Transcrição gerada: ")
+        print(file_path + file_name)
+        print()
+    except:
+        print("Ocoreu um erro ao salvar o texto")
